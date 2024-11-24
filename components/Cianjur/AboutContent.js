2754aee12008b8259c00b6b5de38b317088d0a92
ubeng111 <152304955+ubeng111@users.noreply.github.com>
2024-11-25 05:59:49 +0700

    Update AboutContent.js

--- components/Cianjur/AboutContent.js
+++ components/Cianjur/AboutContent.js
@@ -15,7 +15,7 @@ const AboutContent = ({ city }) => {
           <div className="col-lg-6 col-md-12">
             <div className="about-warp pl-15">
               <span>Tentang Kami</span>
-              <h3>Spesialis SEO Terpercaya di {city}</h3> {/* Judul dinamis berdasarkan kota */}
+              <h3>Spesialis SEO Website Terpercaya di {city}</h3> {/* Judul dinamis berdasarkan kota */}
 
               <p>
                 Di {city}, kami adalah tim yang fokus pada optimasi mesin pencari (SEO) untuk membantu bisnis Anda berkembang secara digital. Kami menggunakan metode yang terbukti efektif, menggabungkan teknik SEO terbaru untuk memastikan website Anda mendapatkan peringkat terbaik di Google dan mesin pencari lainnya.
@@ -26,7 +26,7 @@ const AboutContent = ({ city }) => {
               </p>
 
               <p>
-                Tim kami ahli dalam berbagai teknik SEO, mulai dari riset kata kunci yang sangat relevan, hingga penerapan strategi SEO yang bersifat teknis dan konten. Kami juga mengoptimalkan pengalaman pengguna (UX), memastikan kecepatan situs yang optimal, dan menggunakan backlink yang berkualitas tinggi untuk meningkatkan otoritas situs web Anda. Semua ini kami lakukan untuk memberikan hasil yang nyata dan meningkatkan pendapatan bisnis Anda.
+                Tim kami ahli dalam berbagai teknik SEO Wordpress, mulai dari riset kata kunci yang sangat relevan, hingga penerapan strategi SEO yang bersifat teknis dan konten. Kami juga mengoptimalkan pengalaman pengguna (UX), memastikan kecepatan situs yang optimal, dan menggunakan backlink yang berkualitas tinggi untuk meningkatkan otoritas situs web Anda. Semua ini kami lakukan untuk memberikan hasil yang nyata dan meningkatkan pendapatan bisnis Anda.
               </p>
 
               <div className="about-btn">
