353035405e4480fc5593c3c81ca9d07971a37677
Ubeng111 <akangvillapuncak@gmail.com>
2025-01-03 14:44:56 +0700

    Initial commit

--- components/Website/Features.js
+++ components/Website/Features.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+const spinText = (text) => {
+  const regex = /\{([^\}]+)\}/g;
+  return text.replace(regex, (match, p1) => {
+    const options = p1.split("|");
+    return options[Math.floor(Math.random() * options.length)];
+  });
+};
+
 const Features = ({ city }) => {
   return (
     <>
@@ -12,9 +20,7 @@ const Features = ({ city }) => {
                 <div className="icon">
                   <i className="fas fa-laptop-code"></i> {/* Ikon web design dari Font Awesome */}
                 </div>
-                <h3>
-                  Layanan Pembuatan Website Profesional di {city}
-                </h3>
+                <h3>{spinText(`Layanan Pembuatan Website {Profesional|Berkualitas|Terpercaya} di {city}`)}</h3>
                 <p>
                   Kami menawarkan layanan pembuatan website yang responsif dan menarik untuk bisnis Anda di {city}. Dengan desain yang modern dan fungsionalitas yang sesuai dengan kebutuhan bisnis, website Anda akan memberikan pengalaman pengguna yang luar biasa.
                 </p>
@@ -27,9 +33,7 @@ const Features = ({ city }) => {
                 <div className="icon">
                   <i className="fas fa-shopping-cart"></i> {/* Ikon e-commerce dari Font Awesome */}
                 </div>
-                <h3>
-                  Pembuatan Website E-commerce di {city}
-                </h3>
+                <h3>{spinText(`Pembuatan Website {E-commerce|Toko Online|Platform Jual Beli} di {city}`)}</h3>
                 <p>
                   Kami menyediakan solusi pembuatan website e-commerce di {city}, memungkinkan bisnis Anda untuk menjual produk secara online. Dengan sistem pembayaran yang aman dan fitur keranjang belanja yang mudah digunakan, website e-commerce Anda siap untuk berkembang.
                 </p>
@@ -42,9 +46,7 @@ const Features = ({ city }) => {
                 <div className="icon">
                   <i className="fas fa-mobile-alt"></i> {/* Ikon responsif design dari Font Awesome */}
                 </div>
-                <h3>
-                  Desain Website Responsif untuk Semua Perangkat
-                </h3>
+                <h3>{spinText(`Desain Website {Responsif|Modern|Fleksibel} untuk Semua Perangkat`)}</h3>
                 <p>
                   Layanan pembuatan website kami di {city} fokus pada desain responsif, memastikan website Anda dapat diakses dengan optimal di semua perangkat, baik desktop, tablet, maupun smartphone.
                 </p>
